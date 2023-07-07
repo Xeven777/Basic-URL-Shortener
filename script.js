28ae037e8ccc7bee373b6b649ae2cb535bda6b77
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2023-07-07 12:13:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
-
 function shortenUrl() {
-    var url = document.getElementById("url").value;
-    var custom = document.getElementById("custom").value;
-    var payload = custom ? { "long": url, "custom": custom, "useFallback": true } : { "input": url };
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://gotiny.cc/api");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            var data = JSON.parse(this.responseText);
-            var short = data[0]['code'];
-            var result = document.getElementById("result");
-            result.innerHTML = "Shortened URL's unique code : " + short + "<br>";
-            result.innerHTML += "<a href='https://gotiny.cc/" + short + "' target='_blank'>https://gotiny.cc/" + short + "</a><br>";
-            var link = "https://gotiny.cc/" + short + "\n";
-            var form = document.querySelector("form");
-            form.reset();
-            copyToClipboard(link);
-            result.innerHTML += "Your URL is copied! Just paste it in the browser and reach your destination page! WooHOO!";
-        } else if (this.readyState === XMLHttpRequest.DONE) {
-            var result = document.getElementById("result");
-            result.innerHTML = "Failed to create short URL. Error code: " + this.status;
-        }
-    };
-    xhr.send(JSON.stringify(payload));
+  const url = document.getElementById("url").value;
+  const custom = document.getElementById("custom").value;
+  const payload = custom ? { long: url, custom: custom, useFallback: true } : { input: url };
+
+  fetch("https://gotiny.cc/api", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to create short URL. Error code: " + response.status);
+      }
+    })
+    .then((data) => {
+      const short = data[0].code;
+      const link = `https://gotiny.cc/${short}`;
+
+      displayResult(short, link);
+      copyToClipboard(link);
+    })
+    .catch((error) => {
+      displayError(error.message);
+    });
+}
+
+function displayResult(short, link) {
+  const result = document.getElementById("result");
+  result.innerHTML = `Shortened URL's unique code: ${short}<br>`;
+  result.innerHTML += `<a href="${link}" target="_blank">${link}</a><br>`;
+  result.innerHTML += "Your URL is copied! Just paste it in the browser and reach your destination page! WooHOO!";
+}
+
+function displayError(errorMessage) {
+  const result = document.getElementById("result");
+  result.textContent = errorMessage;
 }
 
 function copyToClipboard(text) {
-    var input = document.createElement("textarea");
-    input.value = text;
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand("Copy");
-    input.remove();
+  const input = document.createElement("textarea");
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("Copy");
+  input.remove();
 }
 
-var form = document.querySelector("form");
+const form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    shortenUrl();
+  event.preventDefault();
+  shortenUrl();
 });
